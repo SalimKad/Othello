7607dad0315583bdc519637cbd6bf7fdec0cfeea
Elisa Gatto <elisa.gatto@live.fr>
2024-02-13 14:11:39 +0100

    avancée interface

---
+++ src/game/BoardInterface.java
@@ -0,0 +1,9 @@
+package game;
+
+public interface BoardMethods {
+    public int getBoardValue(int i, int j);
+
+    public void setBoardValue(int i, int j);
+
+    public void handleClick(int i, int j);
+}
